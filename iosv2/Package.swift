fc077853eca8c7b3817329b6b6f516fc29c6f63c
shubhasai <mshubhasai2002@gmail.com>
2024-09-03 21:30:24 +0530

    iospm config

--- iosv2/Package.swift
+++ iosv2/Package.swift
@@ -1,1 +1,16 @@
-
+import PackageDescription
+let packageName = "nudgecore"
+let package = Package(
+    name: "nudgecore",
+    platforms: [.iOS(.v13)],
+    products: [
+        .library(name: packageName),
+    ],
+    targets: [
+        .binaryTarget(
+            name: packageName,
+            url: "sampleurl",
+            checksum: "7c1f94417f542b95fd52cdb501482709735efd80d4246e3842a5ccbf76e2dc26"
+        ),
+    ]
+)
